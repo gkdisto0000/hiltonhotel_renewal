b2833b37cdb1b1e186c03e580d33b1e43a7c6da7
gkdisto0000 <qufqlcto@naver.com>
2022-06-06 03:01:13 +0900

    ver05

---
+++ js/sub1.js
@@ -0,0 +1,41 @@
+$(function () {
+  // <cnt01 nav>li click anchor, addClass>
+  var cnt01Li = $('.content.cnt01 ul li');
+  var loc = [];
+
+  for (var i = 0; i < $('article').length; i++) {
+    loc[i] = $('article').eq(i).offset().top;
+  }
+  cnt01Li.click(function () {
+    var num = $(this).attr('data-num');
+    $('html').animate({ scrollTop: loc[num] }, 500);
+    cnt01Art.eq(num).addClass('show');
+  });
+
+  var cnt01Art = $('.content.cnt01 .texts');
+  cnt01Art.click(function(){
+    $(this).toggleClass('show');
+  });
+
+  // <Top btn>
+  $('.Top').click(function () {
+    $('html,body').stop().animate({
+      scrollTop: '0'
+    }, 500)
+  });
+  $('.Top').css({
+    opacity: '0'
+  });
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+      $('.Top').css({
+        opacity: '1'
+      });
+    } else {
+      $('.Top').css({
+        opacity: '0'
+      });
+    }
+  });
+});
